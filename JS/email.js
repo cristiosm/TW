4948d10d3597d113c900be104f6aa7ba3dc26ecd
Cristian Magureanu <98318068+cristiosm@users.noreply.github.com>
2023-05-25 06:35:25 +0300

    Add files via upload

---
+++ JS/email.js
@@ -0,0 +1,10 @@
+function openEmailSection() {
+    var emailSection = document.getElementById("email-section");
+    emailSection.style.display = "block";
+  }
+  
+  function closeEmailSection() {
+    var emailSection = document.getElementById("email-section");
+    emailSection.style.display = "none";
+  }
+  
